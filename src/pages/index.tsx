3c3536bcef1227cc59ead2f71703895d7626e1bc
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-08-13 18:29:38 +0200

    Fix HTML mismatch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import { useAppDispatch, RootState as S, setIsDarkMode } from '../redux'
 
 const IndexPage = () => {
-  const isDarkMode = useSelector((s: S) => s.isDarkMode)
+  const isDarkMode = useSelector((s: S) => s.userPreferences.isDarkMode)
   const dispatch = useAppDispatch()
 
   return (
--- src/redux.tsx
+++ src/redux.tsx
@@ -1,5 +1,24 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import {
+  configureStore,
+  createSlice,
+  getDefaultMiddleware,
+  combineReducers,
+} from '@reduxjs/toolkit'
 import { useDispatch, Provider } from 'react-redux'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+
+import storage from './redux-storage'
 
 const userPreferencesSlice = createSlice({
   name: 'userPreferences',
@@ -11,17 +30,43 @@ const userPreferencesSlice = createSlice({
   },
 })
 
+const rootReducer = combineReducers({ userPreferences: userPreferencesSlice.reducer })
+
+const persistConfig = {
+  key: 'persist',
+  storage,
+  whitelist: ['userPreferences'],
+  stateReconciler: hardSet,
+}
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: userPreferencesSlice.reducer,
+  reducer: persistedRootReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      // https://github.com/rt2zz/redux-persist/issues/988#issuecomment-552242978
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 })
 
 export const { setIsDarkMode } = userPreferencesSlice.actions
-
-export type RootState = ReturnType<typeof store.getState>
-
-export type AppDispatch = typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 
-const AppStoreProvider = ({ children }) => <Provider store={store}>{children}</Provider>
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch
+
+const AppStoreProvider = ({ children }) => {
+  const persistor = persistStore(store)
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={children} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  )
+}
 
 export default AppStoreProvider
